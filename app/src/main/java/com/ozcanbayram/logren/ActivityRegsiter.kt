6e0a0ccef386fae4f3e750e28abc45a48b184b84
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-25 19:15:44 +0300

    Register pages UI complated

---
+++ app/src/main/java/com/ozcanbayram/logren/ActivityRegsiter.kt
@@ -0,0 +1,17 @@
+package com.ozcanbayram.logren
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class ActivityRegsiter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regsiter)
+    }
+
+    private fun register(view : View){
+
+    }
+
+}
--- app/src/main/java/com/ozcanbayram/logren/Welcome.kt
+++ app/src/main/java/com/ozcanbayram/logren/Welcome.kt
@@ -1,13 +1,27 @@
 package com.ozcanbayram.logren
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.ozcanbayram.logren.databinding.ActivityMainBinding
 
 class Welcome : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.registerButton.setOnClickListener {
+            val intent = Intent(this@Welcome, ActivityRegsiter::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
     private fun register(view : View){
