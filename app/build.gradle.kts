b4e46850a31739f2ea1f4a512315c513c437ce82
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-25 20:35:02 +0300

    Register page is complated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    //For Firebase
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,4 +51,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //For Firebase
+
+        // Import the BoM for the Firebase platform
+        implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
+        // Add the dependency for the Firebase Authentication library
+        // When using the BoM, you don't specify versions in Firebase library dependencies
+        implementation("com.google.firebase:firebase-auth")
+
+        implementation("com.google.firebase:firebase-analytics")
+
+
 }
--- app/src/main/java/com/ozcanbayram/logren/ActivityRegsiter.kt
+++ app/src/main/java/com/ozcanbayram/logren/ActivityRegsiter.kt
@@ -1,24 +1,50 @@
 package com.ozcanbayram.logren
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.ozcanbayram.logren.databinding.ActivityWelcomeBinding
 import com.ozcanbayram.logren.databinding.ActivityRegsiterBinding
 
 class ActivityRegsiter : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegsiterBinding
+    //For Firebase
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegsiterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
     }
+    fun register(view : View){
+        val email = binding.email.text.toString()
+        val password = binding.password.text.toString()
 
-    private fun register(view : View){
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this@ActivityRegsiter,"Lütfen E-Posta ve Parolanızı eksiksiz giriniz.",Toast.LENGTH_LONG).show()
+        }else {
 
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener { authResult ->
+                val newuser =authResult.user
+                Toast.makeText(this@ActivityRegsiter,"Kayıt Başarılı. Giriş Yapabilirsiniz.",Toast.LENGTH_LONG).show()
+                val intent = Intent(this@ActivityRegsiter,LoginActivity::class.java)
+                startActivity(intent)
+            }.addOnFailureListener {
+                Toast.makeText(this@ActivityRegsiter, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
-
 }
